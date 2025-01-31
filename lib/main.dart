3af3958ea78dd6170a9991f870264152f3f89121
Christopher Miranda <christopher.ramos.miranda@gmail.com>
2025-01-31 05:53:56 -0300

    ajustes de estilização

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,77 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Frases Flutter',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Frases do dia'),
-    );
-  }
+  _HomeState createState() => _HomeState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _HomeState extends State<Home> {
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _numeroAleatorio = 0;
-  final List _frases = [
-    'O importante não pe vencer todos os dias, mas lutar sempre',
-    'É melhor conquistar a si mesmo do que vencer mil batalhas',
-    'O medo de perder tira a vontade de ganhar',
-    'Perder para a razão, sempre é ganhar'
+  final _frases = [
+    "Não espere o futuro mudar tua vida, porque o futuro será a consequência do presente.",
+    "A vida me ensinou a nunca desistir. Nem ganhar, nem perder, mas procurar evoluir.",
+    "Se você cansar, aprenda a descansar e não a desistir.",
+    "O importante não é vencer todos os dias, mas lutar sempre.",
+    "Sua única limitação é aquela que você impõe em sua própria mente.",
   ];
 
-  void _incrementCounter() {
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+
+    var numeroSorteado = Random().nextInt(_frases.length);
+
     setState(() {
-      _numeroAleatorio = Random().nextInt(4);
+      _fraseGerada = _frases[numeroSorteado];
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Pressione o botão para\ngerar uma frase:',
-              style: Theme.of(context).textTheme.titleLarge,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 50,),
-            Text(
-              _frases[_numeroAleatorio],
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-          ],
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                onPressed: _gerarFrase,
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
